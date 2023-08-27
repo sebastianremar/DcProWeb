64b246b927684c462401d83eac1d067eb97c27aa
sebastianremar <rsrdlf93@gmail.com>
2023-08-27 12:13:02 -0400

    fix: remove pics/dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './navigation/nav';
-import Layout from './pages/initial-screen/layout'; // Import the Layout component
-import About from './pages/individual-screens/about/about';
-import Gallery from './pages/individual-screens/gallery/gallery';
-import Reviews from './pages/individual-screens/reviews/reviews';
-import Services from './pages/individual-screens/services/services';
+import NavBar from './client/navigation/nav';
+import Layout from './client/pages/initial-screen/layout'; // Import the Layout component
+import About from './client/pages/individual-screens/about/about';
+import Gallery from './client/pages/individual-screens/gallery/gallery';
+import Reviews from './client/pages/individual-screens/reviews/Reviews';
+import Services from './client/pages/individual-screens/services/services';
 
 const App: React.FC = () => {
   return (
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Interfaces for important components
+export interface Review {
+    reviewRating: string;
+    author: string;
+    reviewBody: string | null;
+    dateCreated: string;
+}
