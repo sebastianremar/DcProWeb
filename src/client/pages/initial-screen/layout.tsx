c2c7a2e5318fad0651db9ff1ac92be70a47e1bad
sebastianremar <rsrdlf93@gmail.com>
2023-09-02 13:18:32 -0400

    testing reviews

--- src/client/pages/initial-screen/layout.tsx
+++ src/client/pages/initial-screen/layout.tsx
@@ -3,15 +3,15 @@ import Portada from './portada/portada';
 import ServicesPortada from './services/services';
 import About from '../individual-screens/about/about';
 import Gallery from '../individual-screens/gallery/gallery';
-import Reviews from '../individual-screens/reviews/Reviews';
+import PortadaReviews from '../initial-screen/reviews/Reviews';
 
 const Layout: React.FC = () => {
   return (
     <div>
       <Portada />
       <ServicesPortada />
+      <PortadaReviews />
       <Gallery />
-      <Reviews />
       <About />
     </div>
   );
---
+++ src/client/pages/initial-screen/reviews/Reviews.tsx
@@ -0,0 +1,52 @@
+import Slider from "react-slick";  
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"; 
+import { Review } from '../../../../config';
+import importedReviews from '../../individual-screens/reviews/Reviews.json';
+import './style.css';
+
+const PortadaReviews: React.FC = () => {
+    const reviews: Review[] = importedReviews.slice(0,9);
+
+    const settings = {
+        dots: false,
+        infinite: true,  // This ensures the looping behavior
+        speed: 500,
+        slidesToShow: 3,  // Display three reviews at once
+        slidesToScroll: 3  // Move three reviews on arrow click
+    };
+
+    const truncateText = (text: string | null, maxLength: number = 150) => {
+        if (!text) return "";  // Handles null or undefined
+        if (text.length <= maxLength) return text;
+        return text.substr(0, maxLength) + "...";
+    }
+      
+
+      return (
+        <div className="portada-reviews-homepage-container">
+            <h1 className="portada-review-header">OUR CUSTOMERS LOVE US</h1>
+            <Slider {...settings}>
+                {reviews.map((review, index) => (
+                    <div key={index} className="slide">
+                        <div className="portada-review-card-homepage">
+                            <div className="portada-author">{review.author}</div>
+                            <div className="portada-stars">
+                                {Array(5).fill(0).map((_, i) => (
+                                    <span key={i} className={i < Math.round(parseFloat(review.reviewRating)) ? 'portada-star filled' : 'portada-star'}>★</span>
+                                ))}
+                            </div>
+                            <div className="portada-body">
+                              {truncateText(review.reviewBody)}
+                              {review.reviewBody && review.reviewBody.length > 150 && <a href="YOUR_URL_HERE" className="portada-review-read-more">Read More</a>}
+                            </div>
+                            <div className="portada-time-ago">{review.dateCreated}</div>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+}
+
+export default PortadaReviews;
