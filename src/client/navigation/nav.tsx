d57e93026e3cf6dee08ea1f946fdef0862fbbfb1
sebastianremar <rsrdlf93@gmail.com>
2023-09-13 18:51:19 -0400

    fix: navbar

--- src/client/navigation/nav.tsx
+++ src/client/navigation/nav.tsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/ductcleanlogo_2.png';
 import { BsFillTelephoneFill as PhoneIcon } from 'react-icons/bs';
+import CustomButton from "../components/CustomButton";
 import './style.css';
 
 const NavBar: React.FC = () => {
   const [scrolled, setScrolled] = useState(false);
 
-  /* 
-    Handles sticky nav. 
-  */
   useEffect(() => {
     const handleScroll = () => {
       const isScrolled = window.scrollY > 25;
@@ -27,34 +25,36 @@ const NavBar: React.FC = () => {
   return (
     <div className="navbar-container">
       <nav className="navbar">
-        <div className="logo">
-          <img src={logo} alt="Logo" />
-        </div>
-        <div className="nav-list" style={{ display: scrolled ? 'none' : 'inline-block' }}>
-          <ul>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/gallery">Gallery</Link></li>
-            <li><Link to="/reviews">Reviews</Link></li>
-          </ul>
-        </div>
-        <div className="book-by-phone-container">
-          <div className="phone-icon">
-            <PhoneIcon />
+        <div className="left-container">
+          <div className="logo">
+            <img src={logo} alt="Logo" />
           </div>
-          <div className="book-number-letter-container">
-            <div className="phone-number">
-              (703)-296-1979
-            </div>
-            <div className="book-now-letter">
-              CALL NOW
-            </div>
+          <div className="nav-list" style={{ display: scrolled ? 'none' : 'flex' }}>
+            <ul>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/services">Services</Link></li>
+              <li><Link to="/gallery">Gallery</Link></li>
+              <li><Link to="/reviews">Reviews</Link></li>
+            </ul>
           </div>
         </div>
-        <div className="book-now-button">
-          <button>
-            Book Now
-          </button>
+        <div className="right-container">
+          <div className="book-by-phone-container">
+            <div className="phone-icon">
+              <PhoneIcon />
+            </div>
+            <div className="book-number-letter-container">
+              <div className="phone-number">
+                (703)-296-1979
+              </div>
+              <div className="book-now-letter">
+                CALL NOW
+              </div>
+            </div>
+          </div>
+          <div className="book-now-button">
+            <CustomButton text="Book Now"/>
+          </div>
         </div>
       </nav>
     </div>
