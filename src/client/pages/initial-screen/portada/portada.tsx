099ad83544200ae707a62b8fb85421ff5450a8f2
sebastianremar <rsrdlf93@gmail.com>
2023-09-13 19:01:16 -0400

    fix: portada

--- src/client/pages/initial-screen/portada/portada.tsx
+++ src/client/pages/initial-screen/portada/portada.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 import './style.css';
 
-
 const Portada: React.FC = () => {
   return (
     <div className="container-portada">
       <div className="portada">
       </div>
-      <div className="book-now">
+      <div className="book-now-portada">
         <p>
           Air Duct & Dryer Vent Cleaning
         </p>
-        <div className="button-container">
-          <button>
-            Book Now
-          </button>
+        <div className="button-container-portada">
+          <button>Book Now</button>
         </div>
       </div>
     </div>
@@ -22,3 +19,4 @@ const Portada: React.FC = () => {
 }
 
 export default Portada;
+
