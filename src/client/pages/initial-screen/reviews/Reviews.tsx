9eb412ad287c93037980c98546798c2000c469e2
sebastianremar <rsrdlf93@gmail.com>
2023-09-15 20:06:01 -0400

    feat: added multiple screen on reviews section

--- src/client/pages/initial-screen/reviews/Reviews.tsx
+++ src/client/pages/initial-screen/reviews/Reviews.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Slider from "react-slick";  
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css"; 
@@ -8,25 +9,47 @@ import './style.css';
 const ReviewsPortada: React.FC = () => {
     const reviews: Review[] = importedReviews.slice(0,9);
 
-    const settings = {
+    const [cardWidth, setCardWidth] = useState('calc(33.33% - 30px)');
+    const [sliderSettings, setSliderSettings] = useState({
         dots: false,
-        infinite: true, 
+        infinite: true,
         speed: 500,
-        slidesToShow: 3, 
-        slidesToScroll: 3  
-    };
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 
     const truncateText = (text: string | null, maxLength: number = 150) => {
         if (!text) return "";  
         if (text.length <= maxLength) return text;
         return text.substr(0, maxLength) + "...";
     }
-      
+
+    useEffect(() => {
+        const updateSliderSettings = () => {
+            if (window.innerWidth <= 980) {
+                setSliderSettings(prev => ({ ...prev, slidesToShow: 1, slidesToScroll: 1 }));
+                setCardWidth('100%');
+            } else if (window.innerWidth <= 1480) {
+                setSliderSettings(prev => ({ ...prev, slidesToShow: 2, slidesToScroll: 2 }));
+                setCardWidth('calc(50% - 50px)');
+            } else {
+                setSliderSettings(prev => ({ ...prev, slidesToShow: 3, slidesToScroll: 3 }));
+                setCardWidth('calc(33.33% - 30px)');
+            }
+        };
+    
+        window.addEventListener('resize', updateSliderSettings);
+        updateSliderSettings(); 
+    
+        return () => {
+            window.removeEventListener('resize', updateSliderSettings);
+        };
+    }, []);
 
       return (
         <div className="portada-reviews-homepage-container">
             <h1 className="portada-review-header">OUR CUSTOMERS LOVE US</h1>
-            <Slider {...settings}>
+            <Slider {...sliderSettings}>
                 {reviews.map((review, index) => (
                     <div key={index} className="slide">
                         <div className="portada-review-card-homepage">
