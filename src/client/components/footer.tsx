758d39c7dd9f18eac2ae982d7fa698f8ad73080f
sebastianremar <rsrdlf93@gmail.com>
2023-10-21 14:32:51 -0400

    fix: order of footer

--- src/client/components/footer.tsx
+++ src/client/components/footer.tsx
@@ -7,15 +7,6 @@ const Footer: React.FC = () => {
     return (
         <footer className="footer">
             <div className="footer-column">
-                <h2>Quick Links</h2>
-                <ul>
-                    <li><Link to="/about" className="link">About</Link></li>
-                    <li><Link to="/services" className="link">Services</Link></li>
-                    <li><Link to="/gallery" className="link">Gallery</Link></li>
-                    <li><Link to="/reviews" className="link">Reviews</Link></li>
-                </ul>
-            </div>
-            <div className="footer-column">
                 <h2>Contact Us</h2>
                 <ul>
                     <li><FaPhoneAlt />  (703) - 296 - 1979</li>
@@ -29,6 +20,15 @@ const Footer: React.FC = () => {
                     <li><a href="https://www.facebook.com/profile.php?id=100029105573482" target="_blank" rel="noreferrer"><FaFacebook /> Facebook</a></li>
                     <li><a href="https://www.instagram.com/duct_clean_pro/" target="_blank" rel="noreferrer"><FaInstagram /> Instagram</a></li>
                     <li><a href="https://www.yelp.com/biz/duct-clean-pro-mclean?override_cta=Request+a+Quote" target="_blank" rel="noreferrer"><FaYelp /> Yelp</a></li>
+                </ul>
+            </div>
+            <div className="footer-column">
+                <h2>Quick Links</h2>
+                <ul>
+                    <li><Link to="/about" className="link">About</Link></li>
+                    <li><Link to="/services" className="link">Services</Link></li>
+                    <li><Link to="/gallery" className="link">Gallery</Link></li>
+                    <li><Link to="/reviews" className="link">Reviews</Link></li>
                 </ul>
             </div>
         </footer>
